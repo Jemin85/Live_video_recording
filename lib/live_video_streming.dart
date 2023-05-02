09fb1974f7cc4ed179f4f4ec5d7455a48d84d3c0
Jemin <jeminsukhadiya2000@gmail.com>
2023-05-02 16:03:57 +0530

    02-05-2023 add live streaming

---
+++ lib/live_video_streming.dart
@@ -0,0 +1,122 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:zego_uikit_prebuilt_live_streaming/zego_uikit_prebuilt_live_streaming.dart';
+
+class LiveVideoStreming extends StatefulWidget {
+  const LiveVideoStreming({super.key});
+
+  @override
+  State<LiveVideoStreming> createState() => _LiveVideoStremingState();
+}
+
+class _LiveVideoStremingState extends State<LiveVideoStreming> {
+  TextEditingController first = TextEditingController();
+  TextEditingController secound = TextEditingController();
+
+  var host = false;
+
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  showMessage(String message) {
+    scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(
+      content: Text(message),
+    ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Live Streaming'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: first,
+              decoration: const InputDecoration(labelText: "User ID"),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: secound,
+              decoration: const InputDecoration(labelText: "User Name"),
+            ),
+            const SizedBox(height: 20),
+            Checkbox(
+                value: host,
+                onChanged: (value) {
+                  setState(() {
+                    host = value!;
+                  });
+                }),
+            const Text("Host"),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => SecoundWidgets(
+                              liveID: "100",
+                              host: host,
+                              userid: first.text,
+                              username: secound.text)));
+                },
+                child: const Text("Join"))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecoundWidgets extends StatefulWidget {
+  final String liveID;
+  final bool host;
+  final String userid;
+  final String username;
+  const SecoundWidgets(
+      {super.key,
+      required this.liveID,
+      required this.host,
+      required this.userid,
+      required this.username});
+
+  @override
+  State<SecoundWidgets> createState() => _SecoundWidgetsState();
+}
+
+class _SecoundWidgetsState extends State<SecoundWidgets> {
+  ZegoUIKitPrebuiltLiveStreamingController controller =
+      ZegoUIKitPrebuiltLiveStreamingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: ZegoUIKitPrebuiltLiveStreaming(
+      appID: 1370142978,
+      appSign:
+          "91398cab5cf9fd39ad035ac1e9749d151f9bed73f0dff6e72278165d8c1ff7cc",
+      userID: widget.userid,
+      liveID: widget.liveID,
+      userName: widget.username,
+      controller: controller,
+      onDispose: () {
+        controller.leave(context, showConfirmation: true);
+        // controller.
+      },
+      config: widget.host
+          ? ZegoUIKitPrebuiltLiveStreamingConfig.host()
+          : ZegoUIKitPrebuiltLiveStreamingConfig.audience(),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'dart:async';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:gallery_saver/gallery_saver.dart';
+
+import 'live_video_streming.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,222 +21,222 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const LiveVideoStreming(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  CameraController? controller;
-  bool isLoading = true;
-  bool isRecording = false;
-  Timer? time;
-  XFile? vPath;
-  Duration _duration = const Duration(seconds: 0);
-  bool timerStrart = false;
+// class _MyHomePageState extends State<MyHomePage> {
+//   CameraController? controller;
+//   bool isLoading = true;
+//   bool isRecording = false;
+//   Timer? time;
+//   XFile? vPath;
+//   Duration _duration = const Duration(seconds: 0);
+//   bool timerStrart = false;
 
-  int clickFirsttime = 0;
+//   int clickFirsttime = 0;
 
-  bool flipCamera = false;
+//   bool flipCamera = false;
 
-  @override
-  void initState() {
-    initCamera();
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     initCamera();
+//     super.initState();
+//   }
 
-  @override
-  void dispose() {
-    time!.cancel();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     time!.cancel();
+//     super.dispose();
+//   }
 
-  initCamera() async {
-    final cameras = await availableCameras();
-    CameraDescription front;
-    if (flipCamera) {
-      front = cameras.firstWhere(
-          (camera) => camera.lensDirection == CameraLensDirection.front);
-    } else {
-      front = cameras.firstWhere(
-          (camera) => camera.lensDirection == CameraLensDirection.back);
-    }
-    controller = CameraController(front, ResolutionPreset.max);
-    await controller!.initialize();
-    setState(() => isLoading = false);
-  }
+//   initCamera() async {
+//     final cameras = await availableCameras();
+//     CameraDescription front;
+//     if (flipCamera) {
+//       front = cameras.firstWhere(
+//           (camera) => camera.lensDirection == CameraLensDirection.front);
+//     } else {
+//       front = cameras.firstWhere(
+//           (camera) => camera.lensDirection == CameraLensDirection.back);
+//     }
+//     controller = CameraController(front, ResolutionPreset.max);
+//     await controller!.initialize();
+//     setState(() => isLoading = false);
+//   }
 
-  String _formatDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, "0");
-    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
-    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
-    String hours = duration.inHours.toString();
-    return "$hours : $twoDigitMinutes : $twoDigitSeconds";
-  }
+//   String _formatDuration(Duration duration) {
+//     String twoDigits(int n) => n.toString().padLeft(2, "0");
+//     String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+//     String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+//     String hours = duration.inHours.toString();
+//     return "$hours : $twoDigitMinutes : $twoDigitSeconds";
+//   }
 
-  void _startTimer() async {
-    setState(() {
-      _duration = const Duration(seconds: 0);
-      timerStrart = true;
-    });
-    await controller!.startVideoRecording();
-    const oneSec = Duration(seconds: 1);
-    time = Timer.periodic(
-      oneSec,
-      (timer) => setState(() {
-        _duration += oneSec;
-      }),
-    );
-  }
+//   void _startTimer() async {
+//     setState(() {
+//       _duration = const Duration(seconds: 0);
+//       timerStrart = true;
+//     });
+//     await controller!.startVideoRecording();
+//     const oneSec = Duration(seconds: 1);
+//     // time = Timer.periodic(
+//     //   oneSec,
+//     //   (timer) => setState(() {
+//     //     _duration += oneSec;
+//     //   }),
+//     // );
+//   }
 
-  void _stopTimer() async {
-    setState(() {
-      timerStrart = false;
-      _duration = const Duration(seconds: 0);
-    });
-    final path = await controller!.stopVideoRecording();
-    setState(() {
-      vPath = path;
-    });
-    time!.cancel();
-  }
+//   void _stopTimer() async {
+//     setState(() {
+//       timerStrart = false;
+//       _duration = const Duration(seconds: 0);
+//     });
+//     final path = await controller!.stopVideoRecording();
+//     setState(() {
+//       vPath = path;
+//     });
+//     time!.cancel();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: const Text("Live Video Recording",style: TextStyle(
-          fontSize: 17,
-          color: Colors.white
-        ),),
-        centerTitle: true,
-      ),
-      body: Stack(
-        // fit: StackFit.expand,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child:
-                controller != null ? CameraPreview(controller!) : Container(),
-          ),
-          Positioned(
-            bottom: 35,
-            left: 20,
-            child: GestureDetector(
-              onTap: () {
-                if (flipCamera) {
-                  initCamera();
-                  setState(() {
-                    flipCamera = false;
-                  });
-                } else {
-                  initCamera();
-                  setState(() {
-                    flipCamera = true;
-                  });
-                }
-              },
-              child: Container(
-                  height: 50,
-                  width: 50,
-                  padding: const EdgeInsets.all(5),
-                  decoration: const BoxDecoration(shape: BoxShape.circle),
-                  child: const Icon(Icons.flip_camera_android,
-                      color: Colors.white, size: 30)),
-            ),
-          ),
-          Positioned(
-              left: 0,
-              right: 0,
-              bottom: 20,
-              child: Column(
-                children: [
-                  if (timerStrart)
-                    Container(
-                        width: 90,
-                        alignment: Alignment.center,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 7, vertical: 5),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.red[500]),
-                        child: Text(
-                          _formatDuration(_duration),
-                          style: const TextStyle(
-                              fontSize: 14,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500),
-                        )),
-                  const SizedBox(height: 15),
-                  GestureDetector(
-                    onTap: () async {
-                      if (timerStrart == false) {
-                        _startTimer();
-                      } else {
-                        _stopTimer();
-                      }
-                    },
-                    child: Container(
-                      height: 80,
-                      width: 80,
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(color: Colors.white, width: 5)),
-                      child: timerStrart
-                          ? const Icon(Icons.pause,
-                              color: Colors.white, size: 40)
-                          : Container(
-                              decoration: const BoxDecoration(
-                                  color: Colors.white, shape: BoxShape.circle),
-                            ),
-                    ),
-                  ),
-                ],
-              )),
-          if (vPath != null)
-            Positioned(
-              bottom: 35,
-              right: 20,
-              child: GestureDetector(
-                onTap: () async {
-                  await GallerySaver.saveVideo(vPath!.path).then(
-                    (value) => ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        behavior: SnackBarBehavior.floating,
-                        backgroundColor: Colors.grey,
-                        content: Text(
-                          "Video Saved",
-                          style: TextStyle(fontSize: 16, color: Colors.white),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-                child: Container(
-                    height: 50,
-                    width: 50,
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: Colors.white, width: 2)),
-                    child: const Icon(Icons.download, color: Colors.white)),
-              ),
-            )
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       extendBodyBehindAppBar: true,
+//       appBar: AppBar(
+//         elevation: 0,
+//         backgroundColor: Colors.transparent,
+//         title: const Text("Live Video Recording",style: TextStyle(
+//           fontSize: 17,
+//           color: Colors.white
+//         ),),
+//         centerTitle: true,
+//       ),
+//       body: Stack(
+//         // fit: StackFit.expand,
+//         children: [
+//           SizedBox(
+//             height: MediaQuery.of(context).size.height,
+//             width: MediaQuery.of(context).size.width,
+//             child:
+//                 controller != null ? CameraPreview(controller!) : Container(),
+//           ),
+//           Positioned(
+//             bottom: 35,
+//             left: 20,
+//             child: GestureDetector(
+//               onTap: () {
+//                 if (flipCamera) {
+//                   initCamera();
+//                   setState(() {
+//                     flipCamera = false;
+//                   });
+//                 } else {
+//                   initCamera();
+//                   setState(() {
+//                     flipCamera = true;
+//                   });
+//                 }
+//               },
+//               child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   padding: const EdgeInsets.all(5),
+//                   decoration: const BoxDecoration(shape: BoxShape.circle),
+//                   child: const Icon(Icons.flip_camera_android,
+//                       color: Colors.white, size: 30)),
+//             ),
+//           ),
+//           Positioned(
+//               left: 0,
+//               right: 0,
+//               bottom: 20,
+//               child: Column(
+//                 children: [
+//                   if (timerStrart)
+//                     Container(
+//                         width: 90,
+//                         alignment: Alignment.center,
+//                         padding: const EdgeInsets.symmetric(
+//                             horizontal: 7, vertical: 5),
+//                         decoration: BoxDecoration(
+//                             borderRadius: BorderRadius.circular(10),
+//                             color: Colors.red[500]),
+//                         child: Text(
+//                           _formatDuration(_duration),
+//                           style: const TextStyle(
+//                               fontSize: 14,
+//                               color: Colors.white,
+//                               fontWeight: FontWeight.w500),
+//                         )),
+//                   const SizedBox(height: 15),
+//                   GestureDetector(
+//                     onTap: () async {
+//                       if (timerStrart == false) {
+//                         _startTimer();
+//                       } else {
+//                         _stopTimer();
+//                       }
+//                     },
+//                     child: Container(
+//                       height: 80,
+//                       width: 80,
+//                       padding: const EdgeInsets.all(5),
+//                       decoration: BoxDecoration(
+//                           shape: BoxShape.circle,
+//                           border: Border.all(color: Colors.white, width: 5)),
+//                       child: timerStrart
+//                           ? const Icon(Icons.pause,
+//                               color: Colors.white, size: 40)
+//                           : Container(
+//                               decoration: const BoxDecoration(
+//                                   color: Colors.white, shape: BoxShape.circle),
+//                             ),
+//                     ),
+//                   ),
+//                 ],
+//               )),
+//           if (vPath != null)
+//             Positioned(
+//               bottom: 35,
+//               right: 20,
+//               child: GestureDetector(
+//                 onTap: () async {
+//                   await GallerySaver.saveVideo(vPath!.path).then(
+//                     (value) => ScaffoldMessenger.of(context).showSnackBar(
+//                       const SnackBar(
+//                         behavior: SnackBarBehavior.floating,
+//                         backgroundColor: Colors.grey,
+//                         content: Text(
+//                           "Video Saved",
+//                           style: TextStyle(fontSize: 16, color: Colors.white),
+//                           textAlign: TextAlign.center,
+//                         ),
+//                       ),
+//                     ),
+//                   );
+//                 },
+//                 child: Container(
+//                     height: 50,
+//                     width: 50,
+//                     padding: const EdgeInsets.all(5),
+//                     decoration: BoxDecoration(
+//                         shape: BoxShape.circle,
+//                         border: Border.all(color: Colors.white, width: 2)),
+//                     child: const Icon(Icons.download, color: Colors.white)),
+//               ),
+//             )
+//         ],
+//       ),
+//     );
+//   }
+// }
